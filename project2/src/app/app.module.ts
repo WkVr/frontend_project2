f3423f3e40d645396f379d2d3d059cd3db223ede
wkvanrensburg <wkvanrensburg@gmail.com>
2020-10-25 15:05:14 +0200

    generate components

--- project2/src/app/app.module.ts
+++ project2/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { UploadComponent } from './upload/upload.component';
+import { RecordsComponent } from './records/records.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    UploadComponent,
+    RecordsComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule
